660825ad3493bb138460549d7d259aad6685527c
MaFls-298 <marialefloress@outlook.com>
2025-09-30 08:16:24 -0600

    xd

---
+++ Clase.java
@@ -0,0 +1,23 @@
+public class Clase{
+    String nombreClase;
+    int hpBase;
+    int manaBase;
+    int multipDano;
+    int multipDefensa;
+
+    public Clase(String nombreClase){
+        this.nombreClase = nombreClase;
+        this.hpBase = 100;
+        this.manaBase = 50;
+        this.multipDano = 1;
+        this.multipDefensa = 1;
+
+    }
+
+    
+
+
+
+
+
+}
